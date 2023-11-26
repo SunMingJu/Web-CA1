275f462c35d3b7d10b4966e8a091416cee00e994
MingJu Sun <20104599@mail.wit.ie>
2023-11-26 11:33:12 +0000

    fix pagination

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -73,9 +73,9 @@ export const getMovieReviews = (id) => {
     });
 };
 
-export const getUpcomingMovies = (page=1) => {
+export const getUpcomingMovies = () => {
   return fetch(
-    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=${page}`
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
   )
     .then((response) => {
             if (!response.ok) {
